da2f529a4c2787d51c14dd52dfe9ea024899e25f
BirajMalakar <BirajMalakar@users.noreply.github.com>
2022-07-29 23:01:53 +0530

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,8 @@
+#abs function is used to get the absolute value of a number such as Integer, Complex and Imaginary.
+print(abs(5))
+print(abs(-5))
+print(abs(5.7))
+print(abs(-5.7))
+print(abs(4j))
+print(abs(-4j))
+print(abs(0+4j))
